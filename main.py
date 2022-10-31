8ef54db293bf2f6951c8742b47b3849412b906da
Arturo Martinez <113930370+amartinez500@users.noreply.github.com>
2022-10-31 17:24:30 +0000

    more functions!!!

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from dynamic_functions import check_3Digits
 #function()
 #returnS()
 #sum = 526 + 345
-result = check_3Digits([55, 99, 6000])
+result = check_3Digits([55, 99, 600, 78, 120, 800 ,343, 242])
 print(result)
